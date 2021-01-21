5c63d5ee750d6b5a31075a4a609b3e2668e8fcc0
Jihyeong Ko <silkstaff@naver.com>
2021-01-21 19:13:30 +0900

    run black

--- example.py
+++ example.py
@@ -1,14 +1,16 @@
 # 추상클래스
 import abc
 
-class Parent(metaclass=abc.ABCMeta):
 
+class Parent(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def method_parent(self):
         pass
 
+
 class Child(Parent):
     def method_child(self):
         return
+
 
 temp = Child()
