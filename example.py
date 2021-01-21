3f3a39e202eceb47357d8287ef3ee235a73aea24
Jihyeong Ko <silkstaff@naver.com>
2021-01-21 19:12:23 +0900

    add summary of abstract class

---
+++ example.py
@@ -0,0 +1,14 @@
+# 추상클래스
+import abc
+
+class Parent(metaclass=abc.ABCMeta):
+
+    @abc.abstractmethod
+    def method_parent(self):
+        pass
+
+class Child(Parent):
+    def method_child(self):
+        return
+
+temp = Child()
