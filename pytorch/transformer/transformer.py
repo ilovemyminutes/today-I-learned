42cf3291fa691cea58acf3312fbe633369a2bb23
Jihyeong Ko <silkstaff@naver.com>
2021-03-04 01:10:22 +0900

    update MultiHeadAttention

--- pytorch/transformer/transformer.py
+++ pytorch/transformer/transformer.py
@@ -4,8 +4,16 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
+
 class Transformer(nn.Module):
-    def __init__(self, vocab_size: int, hidden_dim: int, num_heads: int, num_encoders: int, num_decoders: int):
+    def __init__(
+        self,
+        vocab_size: int,
+        hidden_dim: int,
+        num_heads: int,
+        num_encoders: int,
+        num_decoders: int,
+    ):
         """Transformer for Machine Translation
 
         Args:
@@ -14,37 +22,45 @@ class Transformer(nn.Module):
             num_heads (int): Attention 헤드 수
             num_encoders (int): 인코더 블록 수
             num_decoders (int): 디코더 블록 수
-        """        
+        """
         super(Transformer, self).__init__()
         self.hidden_dim = hidden_dim
-        
-        self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=hidden_dim)
-        self.Encoder = Encoder(hidden_dim=hidden_dim, num_blocks=num_encoders, num_heads=num_heads)
+
+        self.embedding = nn.Embedding(
+            num_embeddings=vocab_size, embedding_dim=hidden_dim
+        )
+        self.Encoder = Encoder(
+            hidden_dim=hidden_dim, num_blocks=num_encoders, num_heads=num_heads
+        )
         self.Decoder = Decoder(num_blocks=num_decoders)
-    
+
     def forward(self, X: torch.Tensor, output, train=True) -> torch.Tensor:
         batch_size = X.size(0)
         max_len = X.size(1)
 
-        data_embedded = self.embedding(X) # (batch_size, max_len, hidden_dim)
+        data_embedded = self.embedding(X)  # (batch_size, max_len, hidden_dim)
         hidden_state = self.Encoder()
-
 
         return
 
 
 class Encoder(nn.Module):
     def __init__(self, hidden_dim: int, num_heads: int, num_blocks: int):
-            
+
         super(Encoder, self).__init__()
 
-        self.w_query = nn.Linear(in_features=hidden_dim, out_features=hidden_dim) # Query
-        self.w_key = nn.Linear(in_features=hidden_dim, out_features=hidden_dim) # Key
-        self.w_value = nn.Linear(in_features=hidden_dim, out_features=hidden_dim) # Value
+        self.w_query = nn.Linear(
+            in_features=hidden_dim, out_features=hidden_dim
+        )  # Query
+        self.w_key = nn.Linear(in_features=hidden_dim, out_features=hidden_dim)  # Key
+        self.w_value = nn.Linear(
+            in_features=hidden_dim, out_features=hidden_dim
+        )  # Value
 
-        self.attention = MultiHeadAttention(hidden_dim=hidden_dim, num_heads=num_heads) # not implemented yet
+        self.attention = MultiHeadAttention(
+            hidden_dim=hidden_dim, num_heads=num_heads
+        )  # not implemented yet
         self.linear = nn.Linear(in_features=hidden_dim, out_features=hidden_dim)
-        
 
     def forward(self, X: torch.Tensor):
         """
@@ -66,7 +82,9 @@ class MultiHeadAttention(nn.Module):
         self.hidden_dim_head = hidden_dim // num_heads
         return
 
-    def forward(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor
+    ) -> torch.Tensor:
         """Query, Key, Value 행렬을 입력 받아 Attention 행렬을 리턴
 
         Args:
@@ -77,22 +95,38 @@ class MultiHeadAttention(nn.Module):
         Returns:
             torch.Tensor: Attention 행렬. (batch_size, max_len, hidden_dim)
         """
-        batch_size = query.size(0)
-        query = query.view(batch_size, -1, self.num_heads, self.hidden_dim_head).transpose(1, 2)
-        key = key.view(batch_size, -1, self.num_heads, self.hidden_dim_head).transpose(1, 2)
-        value = value.view(batch_size, -1, self.num_heads, self.hidden_dim_head).transpose(1, 2)
-        attention_raw = F.softmax(torch.matmul(query, key.transpose(-1, -2)) / math.sqrt(self.hidden_dim))
-        attention_score = torch.matmul(attention_raw, value).view(batch_size, -1, self.hidden_dim)
+        self.batch_size = query.size(0)
+        query = query.view(self.batch_size, -1, self.num_heads, self.hidden_dim_head) # (batch_size, max_len, num_heads, hidden_dim)
+        key = key.view(self.batch_size, -1, self.num_heads, self.hidden_dim_head)
+        value = value.view(self.batch_size, -1, self.num_heads, self.hidden_dim_head)
+
+        query = query.transpose(1, 2)
+        key = key.transpose(1, 2)
+        value = value.transpose(1, 2)
         
+        attention_score = self.get_self_attention(query, key, value)
+
         return attention_score
 
-        
+    def get_self_attention(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor) -> torch.Tensor:
+        attention_raw = F.softmax(
+            torch.matmul(query, key.transpose(-1, -2)) / math.sqrt(self.hidden_dim)
+        )
+        attention_score = torch.matmul(attention_raw, value).view(
+            self.batch_size, -1, self.hidden_dim
+        )
+        return attention_score
+
+    def split(self, X: torch.Tensor)
+
+
+
 class PositionalEncoder(nn.Module):
     def __init__():
         super(PositionalEncoder, self).__init__()
         pass
 
-        
+
 class Decoder(nn.Module):
     def __init__(self, num_blocks: int):
         super(Decoder, self).__init__()
