e04d9c25bee0180f4ceea9fe1ce2424fc7bd51ba
Jihyeong Ko <silkstaff@naver.com>
2021-03-04 00:29:16 +0900

    'wip'

--- pytorch/transformer/README.md
+++ pytorch/transformer/README.md
@@ -1,4 +1,38 @@
-# Transformer from scratch
+# Transformer by myself
+
+## Task
+- Transformer 모델을 활용한 NMT
+
+## Issues
+### 파트별 Input/Output
+- Transformer(overall)
+    - Input: 두 가지 케이스로 나눌 수 있을 듯
+        1. 토큰화된 시퀀스
+            - shape: `(batch_size, max_len)`
+        2. 토큰화도 거치지 않은 문장 자체
+            - Shape: `(batch_size, )`
+    - Output: 번역된 문장
+- Positional Encoding
+    - Input: ???
+        - shape: `(batch_size, max_len, embedding_size)`
+    - Output: ???
+        - shape: `(batch_size, max_len, embedding_size)`
+    - 가장 모르겠는 부분
+        - 학습 가능한 파라미터가 있는가? 있다면 어떻게 구조화해야 하고, 없다면 또 어떻게?
+- Encoder
+    - Input: 임베딩된 입력 x
+        - shape: `(batch_size, max_len, embedding_Size)`
+    - Output: 인코딩 결과
+        - shape: `(batch_size, max_len, embedding_Size)`
+    - Phase #1: Multi-head Attention
+        - Input: 임베딩된 입력 x
+            - shape: `(batch_size, max_len, embedding_Size)`
+        - Output: Attention Matrix
+            - shape: `(batch_size, max_len, embedding_Size)`
+    - Phase #2: Add & Norm
+        - Input: 임베딩된 입력 x, Multi-head Attention의 출력
+        - Output: 입력값이 모두 더해진 뒤 normalization된 값
+            - shape: `(batch_size, max_len, embedding_Size)`
 
 
 > etc
--- pytorch/transformer/transformer.py
+++ pytorch/transformer/transformer.py
@@ -5,23 +5,59 @@ from torch import nn
 from torch.nn import functional as F
 
 class Transformer(nn.Module):
-    def __init__(self, vocab_size, hidden_dim):
+    def __init__(self, vocab_size: int, hidden_dim: int, num_encoders: int, num_decoders: int):
+        """Transformer for Machine Translation
+
+        Args:
+            input (torch.Tensor): 토큰화된 Sequence
+            vocab_size (int): 임베딩 딕셔너리 사이즈
+            hidden_dim (int): 인코더, 디코더 내 hidden state의 차원. 입력 데이터에 대한 임베딩 차원과 같음
+        """        
         super(Transformer, self).__init__()
-        self.vocab_size = vocab_size
         self.hidden_dim = hidden_dim
+        
         self.embedding = nn.Embedding(num_embeddings=vocab_size, embedding_dim=hidden_dim)
-        self.w_q = nn.Linear(in_feauters=self.hidden_dim, out_features=self.hidden_dim)
-        self.w_k = nn.Linear(in_feauters=self.hidden_dim, out_features=self.hidden_dim)
-        self.w_v = nn.Linear(in_feauters=self.hidden_dim, out_features=self.hidden_dim)
+        self.Encoder = Encoder(num_blocks=num_encoders)
+        self.Decoder = Decoder(num_blocks=num_decoders)
     
-    def forward(self, input, output) -> torch.Tensor: 
+    def forward(self, X: torch.Tensor, output, train=True) -> torch.Tensor:
+        batch_size = X.size(0)
+        max_len = X.size(1)
+
+        data_embedded = self.embedding(X) # (batch_size, max_len, hidden_dim)
+        hidden_state = self.Encoder()
+
+
         return
 
-    def get_qkv(self, data):
-        query = self.w_q(data)
-        key = self.w_k(data)
-        value = self.w_v(data)
-        return query, key, value
+
+class Encoder(nn.Module):
+    def __init__(self, hidden_dim: int, num_blocks: int):
+        """
+        """        
+        super(Encoder, self).__init__()
+
+        self.w_query = nn.Linear(in_features=hidden_dim, out_features=hidden_dim) # Query
+        self.w_key = nn.Linear(in_features=hidden_dim, out_features=hidden_dim) # Key
+        self.w_value = nn.Linear(in_features=hidden_dim, out_features=hidden_dim) # Value
+
+        self.attention = MultiHeadAttention(hidden_dim, num_heads) # not implemented yet
+        self.linear = nn.Linear(in_features=hidden_dim, out_features=hidden_dim)
+        
+
+    def forward(self, X: torch.Tensor):
+        """
+        Args:
+            data (torch.Tensor): Positional Encoding 벡터가 더해진 임베딩 벡터. (batch_size, max_len, hidden_dim)
+        """
+        residual = X
+        query = self.w_query(X)
+        key = self.w_key(X)
+        value = self.w_value(X)
+        attention = self.attention(query, key, value)
+
+    def get_qkv(self):
+        raise NotImplementedError()
 
 
 class MultiHeadAttention(nn.Module):
@@ -32,7 +68,7 @@ class MultiHeadAttention(nn.Module):
         self.hidden_split_dim = hidden_dim // num_heads # for multi-head attention
         return
 
-    def forward(self, query, key, value) -> torch.Tensor:
+    def forward(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor) -> torch.Tensor:
         '''
         query, key, value가 입력되는 상황을 전제
         '''
@@ -40,19 +76,21 @@ class MultiHeadAttention(nn.Module):
         attn_value = torch.matmul(attn_score, value)
         return attn_value
 
-class Encoder(nn.Module):
-    def __init__(self):
-        super(Encoder, self).__init__()
-        self.attention = MultiHeadAttention(hidden_dim, num_heads)
-        self.linear = nn.Linear(in_features=hidden_dim, out_features=hidden_dim)
+    def self_attention(self, query: torch.Tensor, key: torch.Tensor, value: torch.Tensor):
+        pass
 
-
-class Decoder(nn.Module):
-    def __init__(self):
-        super(Decoder, self).__init__()
+class PositionalEncoder(nn.Module):
+    def __init__():
+        super(PositionalEncoder, self).__init__()
         pass
 
 
 
 
+
+
         
+class Decoder(nn.Module):
+    def __init__(self, num_blocks: int):
+        super(Decoder, self).__init__()
+        pass
