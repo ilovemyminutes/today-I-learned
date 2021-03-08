7ecf34f21c259e764b82316c27d651941d6e6310
Jihyeong Ko <silkstaff@naver.com>
2021-03-08 19:59:12 +0900

    update transformer.py

--- pytorch/transformer/transformer.py
+++ pytorch/transformer/transformer.py
@@ -49,10 +49,9 @@ class Transformer(nn.Module):
 
     def forward(self, input: torch.Tensor, output, train=True) -> torch.Tensor:
         batch_size = input.size(0)
-
         input_embedded = self._get_embedding_with_positionalencoding(input)
         
-        enc_hidden_state = copy.deepcopy(input_embedded)
+        enc_hidden_state = input_embedded.contiguous()
         for encoder in self.encoders:
             enc_hidden_state = encoder(enc_hidden_state)
         
